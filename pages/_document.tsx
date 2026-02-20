a5b98c7b259d51e0aa66fc04c90a1bbe28210611
Noar <noartmava1@gmail.com>
2026-02-20 21:21:33 +0100

    Add Google verification meta tag

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="BHFvsACrgww4TItleStGj7tu2SPHj9DqmqEOdCP9BnM"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
